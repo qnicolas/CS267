42ae92e095eeedcc82c57fce44753f7bea29cc52
qnicolas <qnicolas@berkeley.edu>
2023-01-29 20:21:00 -0800

    first optimization: loop reordering in do_block, JKI. 16-17%

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -12,16 +12,13 @@ const char* dgemm_desc = "Simple blocked dgemm.";
  * where C is M-by-N, A is M-by-K, and B is K-by-N.
  */
 static void do_block(int lda, int M, int N, int K, double* A, double* B, double* C) {
-    // For each row i of A
-    for (int i = 0; i < M; ++i) {
-        // For each column j of B
-        for (int j = 0; j < N; ++j) {
-            // Compute C(i,j)
-            double cij = C[i + j * lda];
-            for (int k = 0; k < K; ++k) {
+    for (int j = 0; j < N; ++j) {
+        for (int k = 0; k < K; ++k) {
+            for (int i = 0; i < M; ++i) {
+                double cij = C[i + j * lda];
                 cij += A[i + k * lda] * B[k + j * lda];
+                C[i + j * lda] = cij;
             }
-            C[i + j * lda] = cij;
         }
     }
 }
