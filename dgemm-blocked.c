2ae5aed1e32b20a5785cd1573c2e8a55ccd4571a
qnicolas <qnicolas@berkeley.edu>
2023-01-30 15:37:06 -0800

    Much better SIMD microkernel, got to 30% without handling edge cases

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -13,114 +13,82 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
+
 static void micro_kernel(int lda,double* A, double* B, double* C) {
     // Declare
     __m256d Ar0,Ar1,Ar2,Ar3;
     __m256d Br0,Br1,Br2,Br3;
     __m256d Cr0,Cr1,Cr2,Cr3;
     
+    Ar0 = _mm256_loadu_pd(A + 0 * lda);
+    Ar1 = _mm256_loadu_pd(A + 1 * lda);
+    Ar2 = _mm256_loadu_pd(A + 2 * lda);
+    Ar3 = _mm256_loadu_pd(A + 3 * lda);
+    Cr0 = _mm256_loadu_pd(C + 0 * lda);
+    Cr1 = _mm256_loadu_pd(C + 1 * lda);
+    Cr2 = _mm256_loadu_pd(C + 2 * lda);
+    Cr3 = _mm256_loadu_pd(C + 3 * lda);
+
     //// First pass
     // Column 0 of C
-    Cr0 = _mm256_loadu_pd(C + 0 * lda);
-    Ar0 = _mm256_loadu_pd(A + 0 * lda);
     Br0 = _mm256_set1_pd(B[0 + 0 * lda]);
     Cr0 = _mm256_fmadd_pd(Ar0,Br0,Cr0);
-    _mm256_storeu_pd(C + 0 * lda, Cr0);
     // Column 1 of C
-    Cr1 = _mm256_loadu_pd(C + 1 * lda);
-    Ar1 = _mm256_loadu_pd(A + 1 * lda);
     Br1 = _mm256_set1_pd(B[1 + 1 * lda]);
     Cr1 = _mm256_fmadd_pd(Ar1,Br1,Cr1);
-    _mm256_storeu_pd(C + 1 * lda, Cr1);
     // Column 2 of C
-    Cr2 = _mm256_loadu_pd(C + 2 * lda);
-    Ar2 = _mm256_loadu_pd(A + 2 * lda);
     Br2 = _mm256_set1_pd(B[2 + 2 * lda]);
     Cr2 = _mm256_fmadd_pd(Ar2,Br2,Cr2);
-    _mm256_storeu_pd(C + 2 * lda, Cr2);
     // Column 3 of C
-    Cr3 = _mm256_loadu_pd(C + 3 * lda);
-    Ar3 = _mm256_loadu_pd(A + 3 * lda);
     Br3 = _mm256_set1_pd(B[3 + 3 * lda]);
     Cr3 = _mm256_fmadd_pd(Ar3,Br3,Cr3);
-    _mm256_storeu_pd(C + 3 * lda, Cr3);
 
     //// Second pass
     // Column 0 of C
-    Cr0 = _mm256_loadu_pd(C + 0 * lda);
-    Ar3 = _mm256_loadu_pd(A + 3 * lda);
     Br3 = _mm256_set1_pd(B[3 + 0 * lda]);
     Cr0 = _mm256_fmadd_pd(Ar3,Br3,Cr0);
-    _mm256_storeu_pd(C + 0 * lda, Cr0);
     // Column 1 of C
-    Cr1 = _mm256_loadu_pd(C + 1 * lda);
-    Ar0 = _mm256_loadu_pd(A + 0 * lda);
     Br0 = _mm256_set1_pd(B[0 + 1 * lda]);
     Cr1 = _mm256_fmadd_pd(Ar0,Br0,Cr1);
-    _mm256_storeu_pd(C + 1 * lda, Cr1);
     // Column 2 of C
-    Cr2 = _mm256_loadu_pd(C + 2 * lda);
-    Ar1 = _mm256_loadu_pd(A + 1 * lda);
     Br1 = _mm256_set1_pd(B[1 + 2 * lda]);
     Cr2 = _mm256_fmadd_pd(Ar1,Br1,Cr2);
-    _mm256_storeu_pd(C + 2 * lda, Cr2);
     // Column 3 of C
-    Cr3 = _mm256_loadu_pd(C + 3 * lda);
-    Ar2 = _mm256_loadu_pd(A + 2 * lda);
     Br2 = _mm256_set1_pd(B[2 + 3 * lda]);
     Cr3 = _mm256_fmadd_pd(Ar2,Br2,Cr3);
-    _mm256_storeu_pd(C + 3 * lda, Cr3);
 
     //// Third pass
     // Column 0 of C
-    Cr0 = _mm256_loadu_pd(C + 0 * lda);
-    Ar2 = _mm256_loadu_pd(A + 2 * lda);
     Br2 = _mm256_set1_pd(B[2 + 0 * lda]);
     Cr0 = _mm256_fmadd_pd(Ar2,Br2,Cr0);
-    _mm256_storeu_pd(C + 0 * lda, Cr0);
     // Column 1 of C
-    Cr1 = _mm256_loadu_pd(C + 1 * lda);
-    Ar3 = _mm256_loadu_pd(A + 3 * lda);
     Br3 = _mm256_set1_pd(B[3 + 1 * lda]);
     Cr1 = _mm256_fmadd_pd(Ar3,Br3,Cr1);
-    _mm256_storeu_pd(C + 1 * lda, Cr1);
     // Column 2 of C
-    Cr2 = _mm256_loadu_pd(C + 2 * lda);
-    Ar0 = _mm256_loadu_pd(A + 0 * lda);
     Br0 = _mm256_set1_pd(B[0 + 2 * lda]);
     Cr2 = _mm256_fmadd_pd(Ar0,Br0,Cr2);
-    _mm256_storeu_pd(C + 2 * lda, Cr2);
     // Column 3 of C
-    Cr3 = _mm256_loadu_pd(C + 3 * lda);
-    Ar1 = _mm256_loadu_pd(A + 1 * lda);
     Br1 = _mm256_set1_pd(B[1 + 3 * lda]);
     Cr3 = _mm256_fmadd_pd(Ar1,Br1,Cr3);
-    _mm256_storeu_pd(C + 3 * lda, Cr3);
 
     //// Fourth pass
     // Column 0 of C
-    Cr0 = _mm256_loadu_pd(C + 0 * lda);
-    Ar1 = _mm256_loadu_pd(A + 1 * lda);
     Br1 = _mm256_set1_pd(B[1 + 0 * lda]);
     Cr0 = _mm256_fmadd_pd(Ar1,Br1,Cr0);
-    _mm256_storeu_pd(C + 0 * lda, Cr0);
     // Column 1 of C
-    Cr1 = _mm256_loadu_pd(C + 1 * lda);
-    Ar2 = _mm256_loadu_pd(A + 2 * lda);
     Br2 = _mm256_set1_pd(B[2 + 1 * lda]);
     Cr1 = _mm256_fmadd_pd(Ar2,Br2,Cr1);
-    _mm256_storeu_pd(C + 1 * lda, Cr1);
     // Column 2 of C
-    Cr2 = _mm256_loadu_pd(C + 2 * lda);
-    Ar3 = _mm256_loadu_pd(A + 3 * lda);
     Br3 = _mm256_set1_pd(B[3 + 2 * lda]);
     Cr2 = _mm256_fmadd_pd(Ar3,Br3,Cr2);
-    _mm256_storeu_pd(C + 2 * lda, Cr2);
     // Column 3 of C
-    Cr3 = _mm256_loadu_pd(C + 3 * lda);
-    Ar0 = _mm256_loadu_pd(A + 0 * lda);
     Br0 = _mm256_set1_pd(B[0 + 3 * lda]);
     Cr3 = _mm256_fmadd_pd(Ar0,Br0,Cr3);
+
+    //// Store
+    _mm256_storeu_pd(C + 0 * lda, Cr0);
+    _mm256_storeu_pd(C + 1 * lda, Cr1);
+    _mm256_storeu_pd(C + 2 * lda, Cr2);
     _mm256_storeu_pd(C + 3 * lda, Cr3);
 }
 
@@ -147,16 +115,16 @@ static void do_block(int lda, int M, int N, int K, double* A, double* B, double*
     for (int j = 0; j < N; j+=MICROKERNEL_SIZE) {
         for (int k = 0; k < K; k+=MICROKERNEL_SIZE) {
             for (int i = 0; i < M; i+=MICROKERNEL_SIZE) {
-                if ((N-j < MICROKERNEL_SIZE) || (K-k < MICROKERNEL_SIZE) || (M-i < MICROKERNEL_SIZE)){
+                /*if ((N-j < MICROKERNEL_SIZE) || (K-k < MICROKERNEL_SIZE) || (M-i < MICROKERNEL_SIZE)){
                     int M1 = min(MICROKERNEL_SIZE, M - i);
                     int N1 = min(MICROKERNEL_SIZE, N - j);
                     int K1 = min(MICROKERNEL_SIZE, K - k);
                     // Perform individual block dgemm
                     do_dummyblock(lda, M1, N1, K1, A + i + k * lda, B + k + j * lda, C + i + j * lda);
                 }
-                else{
+                else{*/
                     micro_kernel(lda, A + i + k * lda, B + k + j * lda, C + i + j * lda);
-                }
+                //}
             }
         }
     }
@@ -184,3 +152,6 @@ void square_dgemm(int lda, double* A, double* B, double* C) {
         }
     }
 }
+
+
+
